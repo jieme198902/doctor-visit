fe2581a6d4cd6a5478b017737f30a654ee9b9710
jieme198902 <736390v>
2020-08-15 16:26:58 +0800

    初始化提交代码

--- src/main/java/com/doctor/visit/service/UploadService.java
+++ src/main/java/com/doctor/visit/service/UploadService.java
@@ -5,12 +5,9 @@ import com.doctor.visit.domain.BusFile;
 import com.doctor.visit.repository.BusFileMapper;
 import com.doctor.visit.web.rest.util.IDKeyUtil;
 import com.doctor.visit.web.rest.util.Utils;
-import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,15 +17,12 @@ import org.springframework.web.multipart.support.StandardMultipartHttpServletReq
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
 
 
 /**
  * 文件上传
  */
 @Service
-@EnableAsync
 public class UploadService {
     private static final Logger logger = LoggerFactory.getLogger(UploadService.class);
 
@@ -48,55 +42,49 @@ public class UploadService {
      * @param request
      * @return
      */
-    @Async
     public void uploadFiles(BusFile busFile, HttpServletRequest request) {
+        String folder = busFile.getBus();
+        File dirRootPath = new File((rootPath.endsWith(Constants.SLASH) ? rootPath : (rootPath + Constants.SLASH)) + folder);
+        if (!dirRootPath.exists()) {
+            dirRootPath.mkdirs();
+        }
+        if (busFile.isDelBefore()) {
+            BusFile delRecord = new BusFile();
+            delRecord.setBusId(busFile.getBusId());
+            delRecord.setBus(busFile.getBus());
+            delRecord.setFileType(busFile.getFileType());
+            busFileMapper.delete(delRecord);
+        }
+
         if (request instanceof StandardMultipartHttpServletRequest) {
             MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
             MultiValueMap<String, MultipartFile> multiValueMap = multipartHttpServletRequest.getMultiFileMap();
-            List<MultipartFile> fileList = Lists.newArrayList();
-            multiValueMap.forEach((s, files) -> fileList.addAll(files));
-
-            if (fileList.isEmpty()) {
-                return;
-            }
-            String folder = busFile.getBus();
-
-            File dirRootPath = new File((rootPath.endsWith(Constants.SLASH) ? rootPath : (rootPath + Constants.SLASH)) + folder);
-            if (!dirRootPath.exists()) {
-                dirRootPath.mkdirs();
-            }
-            if(busFile.isDelBefore()){
-                BusFile delRecord = new BusFile();
-                delRecord.setBusId(busFile.getBusId());
-                delRecord.setBus(busFile.getBus());
-                delRecord.setFileType(busFile.getFileType());
-                busFileMapper.delete(delRecord);
-            }
-            for (MultipartFile multipartFile : fileList) {
-                // 获取文件的原名称
-                String fileName = multipartFile.getOriginalFilename().replace(".temp", "");
-                //文件后缀
-                String suffix = fileName.substring(fileName.lastIndexOf(Constants.POINT));
-                String newFileName = Utils.generateOfDate() + Constants.UNDERLINE + Utils.generateOf8() + suffix;
-                File destFile = new File(dirRootPath, newFileName);
-                try {
-//                    multipartFile.transferTo(destFile);
-//                    FileUtils.copyInputStreamToFile(multipartFile.getInputStream(),destFile);
-                    Files.copy(multipartFile.getInputStream(),destFile.toPath());
-
-                    try {
-                        Runtime.getRuntime().exec("chmod -R 755 " + destFile.getAbsolutePath());
-                    } catch (IOException e) {
-                        e.printStackTrace();
+            multiValueMap.forEach((s, files) -> {
+                if (null != files && !files.isEmpty()) {
+                    for (MultipartFile multipartFile : files) {
+                        // 获取文件的原名称
+                        String fileName = multipartFile.getOriginalFilename().replace(".temp", "");
+                        //文件后缀
+                        String suffix = fileName.substring(fileName.lastIndexOf(Constants.POINT));
+                        String newFileName = Utils.generateOfDate() + Constants.UNDERLINE + Utils.generateOf8() + suffix;
+                        File destFile = new File(dirRootPath, newFileName);
+                        try {
+                            multipartFile.transferTo(destFile);
+                            try {
+                                Runtime.getRuntime().exec("chmod -R 755 " + destFile.getAbsolutePath());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        String abPath = folder + Constants.SLASH + newFileName;
+                        busFile.setId(IDKeyUtil.generateId());
+                        busFile.setFilePath(abPath);
+                        busFileMapper.insertSelective(busFile);
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
-                String abPath = folder + Constants.SLASH + newFileName;
-                busFile.setId(IDKeyUtil.generateId());
-                busFile.setFilePath(abPath);
-                busFileMapper.insertSelective(busFile);
-            }
+            });
         }
     }
 }
